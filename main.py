8d13ae00943e23763f8f6de1e5be87ef5431dd3d
Hugo Pavy <hugo.pavy@gmail.com>
2026-03-23 15:03:53 +0100

    poetry changes

--- main.py
+++ main.py
@@ -14,38 +14,38 @@ if __name__ == "__main__":
     config = load_config("config.yaml")
     set_seed(config.seed)
     
-    for img in config.test_images:
-        print(f"Processing image: {img}")
-        # Load the noisy image and the ground truth image
-        img_L_tensor = ...  # Load the noisy image as a tensor
-        img_H_tensor = ...  # Load the ground truth image as a tensor
+    # for img in config.test_images:
+    #     print(f"Processing image: {img}")
+    #     # Load the noisy image and the ground truth image
+    #     img_L_tensor = ...  # Load the noisy image as a tensor
+    #     img_H_tensor = ...  # Load the ground truth image as a tensor
 
-        # Denoising process using the diffusion model
-        x_0 = ...  # Denoised image obtained from the diffusion model
+    #     # Denoising process using the diffusion model
+    #     x_0 = ...  # Denoised image obtained from the diffusion model
 
-    ### Phase De Test
-        loss_fn_vgg = lpips.LPIPS(net='vgg').to(device)
+    # ### Phase De Test
+    #     loss_fn_vgg = lpips.LPIPS(net='vgg').to(device)
 
-        test_results = dict()  
-        test_results['lpips'] = []   
-        test_results['psnr'] = []
-        test_results['fid'] = []
+    #     test_results = dict()  
+    #     test_results['lpips'] = []   
+    #     test_results['psnr'] = []
+    #     test_results['fid'] = []
 
 
-        fid_score = calculate_fid(denoised_images, ground_truth_images) ## tensor in range [0, 255]
-        test_results['fid'].append(fid_score)
-        lpips_score = loss_fn_vgg(x_0.detach()*2-1, img_H_tensor) ## tensor in range [-1, 1]
-        test_results['lpips'].append(lpips_score.item())
-        psnr_score = calculate_psnr(img_H_tensor.cpu().numpy(), x_0.cpu().numpy()) ## numpy array in range [0, 255]
-        test_results['psnr'].append(psnr_score)
+    #     fid_score = calculate_fid(denoised_images, ground_truth_images) ## tensor in range [0, 255]
+    #     test_results['fid'].append(fid_score)
+    #     lpips_score = loss_fn_vgg(x_0.detach()*2-1, img_H_tensor) ## tensor in range [-1, 1]
+    #     test_results['lpips'].append(lpips_score.item())
+    #     psnr_score = calculate_psnr(img_H_tensor.cpu().numpy(), x_0.cpu().numpy()) ## numpy array in range [0, 255]
+    #     test_results['psnr'].append(psnr_score)
 
-    ave_psnr = sum(test_results['psnr']) / len(test_results['psnr'])
-    ave_lpips = sum(test_results['lpips']) / len(test_results['lpips'])
-    ave_fid = sum(test_results['fid']) / len(test_results['fid'])
+    # ave_psnr = sum(test_results['psnr']) / len(test_results['psnr'])
+    # ave_lpips = sum(test_results['lpips']) / len(test_results['lpips'])
+    # ave_fid = sum(test_results['fid']) / len(test_results['fid'])
 
-    print("Average PSNR:", ave_psnr)
-    print("Average LPIPS:", ave_lpips)
-    print("Average FID:", ave_fid)
+    # print("Average PSNR:", ave_psnr)
+    # print("Average LPIPS:", ave_lpips)
+    # print("Average FID:", ave_fid)
     image = load_image(config.path_to_image)  # 269x269x3
     read_and_save(image, config.path_to_save)
     print("piche")

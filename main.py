7ee3dad73f70186d65e8664dc21c1b3ac9d66fce
Hugo Pavy <hugo.pavy@gmail.com>
2026-03-23 14:23:39 +0100

    solving bug

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """The main code. You can modify config.yaml to change the parameters to run the code"""
 
-from pnp_denoising_diffusion.utils import load_config, set_seed
+from pnp_denoising_diffusion.utils.utils import load_config, set_seed
 
 if __name__ == "__main__":
     config = load_config("config.yaml")

cf82b4abaf6f2c6cc2bf886bcaf571333ffa1937
valentinexbrayat <valentin.exbrayat@etu.minesparis.psl.eu>
2026-03-24 15:16:33 +0000

    modèle ffhq fonctionne et itération sur image 69000 marche

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """The main code. You can modify config.yaml to change the parameters to run the code"""
 
 import torch
+import cv2
 import numpy as np 
 from pnp_denoising_diffusion.utils.score import calculate_psnr, calculate_fid_process   
 import lpips
@@ -34,9 +35,22 @@ if __name__ == "__main__":
     t_start = config.num_train_timesteps - 1 
 
     image = load_image(config.path_to_image)  # 269 x 269 x 3
-    image = image[:256, :256, :] # 256 x 256 x 3
-    image_transformed, mask = transform_image(image, config)
+    image = load_image(config.path_to_image)  # [H, W, 3]
+    h, w = image.shape[:2]
     
+    # Downsample si l'image est trop grande (> 512)
+    if h > 512 or w > 512:
+        scale = 256 / min(h, w)
+        new_h, new_w = int(h * scale), int(w * scale)
+        image = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_AREA)
+        h, w = new_h, new_w
+        
+    # Center crop au lieu d'un crop en haut à gauche pour garder le sujet centré
+    y_start = max(0, (h - 256) // 2)
+    x_start = max(0, (w - 256) // 2)
+    image = image[y_start:y_start+256, x_start:x_start+256, :]
+
+    image_transformed, mask = transform_image(image, config)
 
     # transfering to shape 1x3x256x256 and mapping to [-1, 1]
     image = torch.from_numpy(np.ascontiguousarray(image)).permute(2, 0, 1).float().unsqueeze(0).to(device)
@@ -91,56 +105,59 @@ if __name__ == "__main__":
     imshow(x, title='init random', save_path="results/init_random.png", show=False)
     imshow(y, title='image transformé', save_path="results/Image_intiale_Transformée.png", show=False)
 
+    torch.cuda.empty_cache()
 
     print(f"--- Reverse Diffusion --- {len(seq)} ")
     # reverse diffusion for one image from random noise
-    for i in range(len(seq)):
-        print(i)
-        curr_sigma = sigmas[seq[i]].cpu().numpy()
-        t_i = utils_model.find_nearest(reduced_alpha_cumprod, curr_sigma)
-        
-        # skip iters
-        if t_i > t_start:
-            continue
+    with torch.no_grad():
+        for i in range(len(seq)):
+            print(i)
+            curr_sigma = sigmas[seq[i]].cpu().numpy()
+            t_i = utils_model.find_nearest(reduced_alpha_cumprod, curr_sigma)
+            
+            # skip iters
+            if t_i > t_start:
+                continue
 
-        # Déterminer le prochain timestep pour le saut DDIM
-        t_im1 = utils_model.find_nearest(reduced_alpha_cumprod, sigmas[seq[min(i+1, len(seq)-1)]].cpu().numpy())
+            # Déterminer le prochain timestep pour le saut DDIM
+            t_im1 = utils_model.find_nearest(reduced_alpha_cumprod, sigmas[seq[min(i+1, len(seq)-1)]].cpu().numpy())
 
-        # -------------------------------------------------------
-        # SOLUTION ANALYTIQUE ET SAUT DDIM (DiffPIR)
-        # -------------------------------------------------------
-        if i < (config.num_train_timesteps - noise_model_t):
-            x_next, x0_est = single_diffpir_step(
-                x, y, mask, t_i, t_im1, model, rhos, sigmas, alphas_cumprod, config.guidance_scale
-            )
-            x = x_next
-        else:
-            x_next, _ = simple_diffusion_step(model, x, t_i, t_im1, alphas_cumprod, eta=0.0)
-            x = x_next
+            # -------------------------------------------------------
+            # SOLUTION ANALYTIQUE ET SAUT DDIM (DiffPIR)
+            # -------------------------------------------------------
+            if i < (config.num_train_timesteps - noise_model_t):
+                x_next, x0_est = single_diffpir_step(
+                    x, y, mask, t_i, t_im1, model, rhos, sigmas, alphas_cumprod, config.guidance_scale, zeta = config.zeta
+                )
+                x = x_next
+            else:
+                x_next, _ = simple_diffusion_step(model, x, t_i, t_im1, alphas_cumprod, eta=0.0)
+                x = x_next
 
-            # # -------------------------------------------------------
-            # # STEP 4: RE-BRUITAGE (Si itérations U > 1)
-            # # -------------------------------------------------------
-            # # Si on n'est pas à la dernière itération interne, on "remonte" le bruit
-            # if u < config.iter_num_U - 1 and seq[i] != seq[-1]:
-            #     # On utilise le ratio d'alphas pour rajouter la dose exacte de bruit
-            #     sqrt_alpha_effective = torch.sqrt(config.alphas_cumprod[t_i] / config.alphas_cumprod[t_im1])
-            #     noise_to_add = torch.sqrt(
-            #         (1 - config.alphas_cumprod[t_i]) - (sqrt_alpha_effective**2 * (1 - config.alphas_cumprod[t_im1]))
-            #     )
-            #     x = sqrt_alpha_effective * x + noise_to_add * torch.randn_like(x)
+                # # -------------------------------------------------------
+                # # STEP 4: RE-BRUITAGE (Si itérations U > 1)
+                # # -------------------------------------------------------
+                # # Si on n'est pas à la dernière itération interne, on "remonte" le bruit
+                # if u < config.iter_num_U - 1 and seq[i] != seq[-1]:
+                #     # On utilise le ratio d'alphas pour rajouter la dose exacte de bruit
+                #     sqrt_alpha_effective = torch.sqrt(config.alphas_cumprod[t_i] / config.alphas_cumprod[t_im1])
+                #     noise_to_add = torch.sqrt(
+                #         (1 - config.alphas_cumprod[t_i]) - (sqrt_alpha_effective**2 * (1 - config.alphas_cumprod[t_im1]))
+                #     )
+                #     x = sqrt_alpha_effective * x + noise_to_add * torch.randn_like(x)
 
-        current_x0 = x0_est if 'x0_est' in locals() else x
-        x_0_progress = (current_x0 / 2 + 0.5)
+            current_x0 = x0_est if 'x0_est' in locals() else x
+            x_0_progress = (current_x0 / 2 + 0.5)
 
-        if (seq[i] in progress_seq):
-            x_show = x_0_progress.clone().detach().cpu().numpy()       #[0,1]
-            x_show = np.squeeze(x_show)
-            if x_show.ndim == 3:
-                x_show = np.transpose(x_show, (1, 2, 0))
-            imshow(x_show, title=f'Denoised Image {i}', save_path=f"results/etape_{i}.png", show=False)
-            progress_img.append(x_show)
-        
+            if (seq[i] in progress_seq):
+                x_show = x_0_progress.clone().detach().cpu().numpy()       #[0,1]
+                x_show = np.squeeze(x_show)
+                if x_show.ndim == 3:
+                    x_show = np.transpose(x_show, (1, 2, 0))
+                imshow(x_show, title=f'Denoised Image {i}', save_path=f"results/etape_{i}.png", show=False)
+                progress_img.append(x_show)
+            torch.cuda.empty_cache()
+            
     #recover intial ground truth image
     x[mask.to(torch.bool)] = y[mask.to(torch.bool)]
 
@@ -159,7 +176,8 @@ if __name__ == "__main__":
     
     # image_uint8 converts from [-1, 1] to [0, 255]
     image_uint8 = ((image / 2 + 0.5) * 255).clamp(0, 255).to(torch.uint8)
-    
+    x_0_uint8 = (x_0_output * 255).clamp(0, 255).to(torch.uint8)
+
     img_psnr_gt = np.transpose(image_uint8.squeeze(0).cpu().numpy(), (1, 2, 0)) # [0, 255] HWC
     img_psnr_est = np.transpose(x_0_uint8.squeeze(0).cpu().numpy(), (1, 2, 0)) # [0, 255] HWC
 
